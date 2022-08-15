bc6bde89556b892d10fe02dbd2a30c4d64112c43
LAPTOP-25KFC7M6\teloc <telocim@yahoo.com>
2022-08-15 01:28:07 -0700

    progress check in

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private Transform cam;
+    [SerializeField] private float relativeMove = .3f;
+
+    private void Update()
+    {
+        transform.position = new Vector2(cam.position.x * relativeMove, cam.position.y * relativeMove);
+    }
+
+}
